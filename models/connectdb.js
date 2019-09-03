96c002704c3b915935004fdec70297a06a8e2179
mookiie <mookiie.343@gmail.com>
2019-09-03 10:30:31 +0700

    edit database

--- models/connectdb.js
+++ models/connectdb.js
@@ -5,7 +5,7 @@ var mssql = require('mssql')
 const config = {
     user: 'dev01',
     password: 'D3v0!',
-    server: '172.18.21.20', 
+    server: '172.18.60.3', 
     database: 'kpi_ticket',
     options: {
         encrypt: true // Use this if you're on Windows Azure
@@ -22,7 +22,7 @@ module.exports = {
             if (err) callback(err);
             mssql.query(sql, function (err, result, fields) {
                 if (err){
-                    console.log(moment().format('DD/MM/YYYY HH:mm:ss') + " - Error : " + err);                    
+                    console.log(moment().format('DD/MM/YYYY HH:mm:ss') + " - Error : " + err);
                     callback(err);
                   };
                 mssql.close( function(err){
